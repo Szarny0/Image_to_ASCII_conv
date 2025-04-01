99f2a9828591bd9080ec1dc09b95a3349a2cc6cd
Szarny0 <asdf.ftudgsdv@djjfp.dshg>
2025-04-01 21:17:21 +0200

    conversion_capacity

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "image.hpp"
+#include <fstream>
 using namespace std;
 
 int main (void){
+/*
     const int width = 640;
     const int height = 480;
     Image image(width, height);
@@ -15,11 +17,32 @@ int main (void){
         }    
     }
    image.Export("image.bmp"); 
-   
-   Image copy(0, 0);
+*/   
+    Image copy(0, 0);
+    const string ASCIICHARS = "                           `^”,:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+    int strLen = ASCIICHARS.size();
+    copy.Read("A.bmp");
+    std::ofstream f;
+    f.open("ascii.txt", std::ios::out);
+    for (int y = 11; y >= 0; y--)
+    {
+        for (int x = 10; x >= 0; x--)
+        {
+        const Color pixel = copy.GetColor(x, y);
+        float bright = (0.2126 * pixel.r) + (0.7152 * pixel.g) + (0.0722 * pixel.b);
 
-   copy.Read("image.bmp");
 
-   copy.Export("copy.bmp");
+        float real = bright * (float)(strLen-1);
+        //cout << bright << endl;
+        int number = (int)real;
+        
+
+        char c = ASCIICHARS[number];
+        f << c; 
+        }
+        f << endl;
+    }
+    f.close();
+ //  copy.Export("copy.bmp");
 }
 
